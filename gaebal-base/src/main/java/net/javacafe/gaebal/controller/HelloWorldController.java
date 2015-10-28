9277ac01d367aed2234e893102ecbc9ed695696b
HyungTae Tyler Lim <placebo2me@gmail.com>
2015-10-28 23:52:01 +0900

    JSP 프로젝트 구성i

--- gaebal-base/src/main/java/net/javacafe/gaebal/controller/HelloWorldController.java
+++ gaebal-base/src/main/java/net/javacafe/gaebal/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package net.javacafe.gaebal.controll;
+package net.javacafe.gaebal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
